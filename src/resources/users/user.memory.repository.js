226256dfe9ccb04bf4f719a580e1ca7b6d3f6136
vera <verapoles1@mail.ru>
2021-05-15 11:56:33 +0200

    feat: implement user

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,15 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
-};
+const User = require('../../common/db');
 
-module.exports = { getAll };
+const getAllUsers = () => User.getAllUsers();
+const getUser = (id) => User.getUser(id);
+const postUser = (user) => User.postUser(user);
+const putUser = (id, user) => User.putUser(id, user);
+const deleteUser = (id) => User.deleteUser(id);
+
+module.exports = {
+  getAllUsers,
+  getUser,
+  postUser,
+  putUser,
+  deleteUser
+};
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,15 @@
 const usersRepo = require('./user.memory.repository');
 
-const getAll = () => usersRepo.getAll();
+const getAllUsers = () => usersRepo.getAllUsers();
+const getUser = (id) => usersRepo.getUser(id);
+const postUser = (user) => usersRepo.postUser(user);
+const putUser = (id, user) => usersRepo.putUser(id, user);
+const deleteUser = (id) => usersRepo.deleteUser(id);
 
-module.exports = { getAll };
+module.exports = {
+    getAllUsers,
+    getUser,
+    postUser,
+    putUser,
+    deleteUser
+};
