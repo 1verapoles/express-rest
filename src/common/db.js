849156b439537f869e7e6fd49fac922b81465d68
vera <verapoles1@mail.ru>
2021-05-29 18:51:04 +0200

    docs: user docs

--- src/common/db.js
+++ src/common/db.js
@@ -2,31 +2,59 @@ const User = require('../resources/users/user.model');
 const Board = require('../resources/boards/board.model');
 const Task = require('../resources/tasks/task.model');
 
+/**
+ * @type {{users: User[], boards: Board[], tasks: Task[]}} 
+ */
 const db = { users: [], boards: [], tasks: [] };
 
+/**
+ * Returns the array of all users
+ * @returns {User[]} all users
+ */
 const getAllUsers = () => db.users;
 
+/**
+ * Returns the user by specified id or an error if user not found
+ * @param {string} id  id of user
+ * @returns {(User|Object)} user or error object if an error occurred
+ */
 const getUser = (id) => {
   const user = db.users.find(el => el.id === id);
   if (!user) throw new Error(`User with id ${id} not found`);
   return user;
 };
 
+/**
+ * Creates new user and adds him to db 
+ * @param {{name: string, login: string, password: string}} user data for new user
+ * @returns {User} new user 
+ */
 const postUser = (user) => {
   const newUser = new User({ ...user });
   db.users.push(newUser);
   return newUser;
 };
 
+/**
+ * Changes user's data in db or returns an error if user not found
+ * @param {string} id  id of user 
+ * @param {{name: string, login: string, password: string}} user data for user's update
+ * @returns {(User|Object)} user with changed data or an error if user not found
+ */
 const putUser = (id, user) => {
   const userUpdate = db.users.find(el => el.id === id);
   if (!userUpdate) throw new Error(`User with id ${id} not found`);
-  userUpdate.name = user.name;
-  userUpdate.login = user.login;
-  userUpdate.password = user.password;
+  if (user.name) {userUpdate.name = user.name;}
+  if (user.login) {userUpdate.login = user.login;}
+  if (user.password) {userUpdate.password = user.password;}
   return userUpdate;
 };
 
+/**
+ * Deletes the user by specified id or returns an error if user not found
+ * @param {string} id  id of user
+ * @returns {(void|Object)} returns nothing or error object if an error occurred
+ */
 const deleteUser = (id) => {
   const userIndex = db.users.findIndex(el => el.id === id);
   if (userIndex === -1) throw new Error(`User with id ${id} not found`);
