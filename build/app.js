ec00924ec534279baa8128abd1e309ae82e1dca1
vera <verapoles1@mail.ru>
2021-06-06 07:51:09 +0200

    feat: implement logging

--- build/app.js
+++ build/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
+const { writeInfoLog, writeErrorLog } = require('./common/middleware/winstonConfig');
+const errorHandler = require('./common/middleware/errorHandler');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
@@ -11,6 +13,10 @@ const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 app.use(express.json());
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use((req, res, next) => {
+    writeInfoLog(req, res);
+    next();
+});
 app.use('/', (req, res, next) => {
     if (req.originalUrl === '/') {
         res.send('Service is running!');
@@ -21,12 +27,15 @@ app.use('/', (req, res, next) => {
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 boardRouter.use('/:boardId/tasks', taskRouter);
-app.use('/docs', express.static(path.join(__dirname, 'docs')));
-app.use((err, _req, res, next) => {
-    if (err) {
-        const { name, message, stack } = err;
-        res.status(404).json({ name, message, stack });
-    }
-    next();
+app.use(errorHandler);
+process.on('uncaughtException', err => {
+    writeErrorLog(err);
+    process.exit(1);
 });
+//throw new Error('uncaughtException: Oops!');
+process.on('unhandledRejection', err => {
+    writeErrorLog(err);
+    process.exit(1);
+});
+// Promise.reject(Error('unhandledRejection: Oops!'));
 module.exports = app;
